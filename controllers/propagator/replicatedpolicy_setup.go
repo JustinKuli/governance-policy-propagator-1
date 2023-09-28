7e754a299ad6f4f3bac03534a4717b36e258bff7
Justin Kulikauskas <jkulikau@redhat.com>
2023-09-28 16:15:47 -0400

    Add concurrency to policy reconciles
    This increases the root policy concurrency from 1 to 2, and makes it
    configurable. It also adds concurrency to the replicated policy - the
    default is 10, up from the concurrency it replaces which defaulted to 5.
    
    Some refactoring was done to have all reconciler concurrencies be
    configured uniformly.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- controllers/propagator/replicatedpolicy_setup.go
+++ controllers/propagator/replicatedpolicy_setup.go
@@ -11,6 +11,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -22,9 +23,10 @@ import (
 )
 
 func (r *ReplicatedPolicyReconciler) SetupWithManager(
-	mgr ctrl.Manager, dynWatcherSrc source.Source, updateSrc source.Source,
+	mgr ctrl.Manager, maxConcurrentReconciles uint, dynWatcherSrc source.Source, updateSrc source.Source,
 ) error {
 	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{MaxConcurrentReconciles: int(maxConcurrentReconciles)}).
 		Named("replicated-policy").
 		For(
 			&policiesv1.Policy{},
